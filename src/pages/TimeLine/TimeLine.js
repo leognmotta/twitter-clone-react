14737eeea869016ba2ec73b2eeb9bb284ae0a188
Leonardo Motta <leo@domboscoatacado.com.br>
2019-02-06 14:56:18 -0200

    Add socket.io update

--- src/pages/TimeLine/TimeLine.js
+++ src/pages/TimeLine/TimeLine.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import socket from 'socket.io-client';
 
 import Tweet from '../../components/Tweet/Tweet';
 import twitterLogo from '../../twitter.svg';
@@ -12,10 +13,28 @@ class TimeLine extends Component {
   };
 
   async componentDidMount() {
+    this.subscribeToEvents();
+
     const tweets = await api.get('/tweets');
 
     this.setState({ tweets: tweets.data });
   }
+
+  subscribeToEvents = () => {
+    const io = socket('http://localhost:8080');
+
+    io.on('tweet', data => {
+      this.setState({ tweets: [data, ...this.state.tweets] });
+    });
+
+    io.on('like', data => {
+      this.setState({
+        tweets: this.state.tweets.map(tweet =>
+          tweet._id === data._id ? data : tweet
+        )
+      });
+    });
+  };
 
   newTweetHandler = async event => {
     if (event.keyCode !== 13) return;
